452da1e1f0376fed3f915327c5d238d98647580d
Daniel Muñoz <daniel.munoz.infante.tech@bbva.com>
2025-01-28 22:01:33 +0100

    añadido lista de la compra

--- react-sample/src/main.jsx
+++ react-sample/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import GroceryApp from "./grocery/App";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <br></br>
+    <h1>----------------------------</h1>
+    <GroceryApp />
+  </StrictMode>
+);
