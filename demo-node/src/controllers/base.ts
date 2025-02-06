bc00b9bf907d188ffe9a26b6d6dd405e6114fd59
Daniel Muñoz <daniel.munoz.infante.tech@bbva.com>
2025-02-06 19:49:57 +0100

    base

--- demo-node/src/controllers/base.ts
+++ demo-node/src/controllers/base.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 export async function base(req: Request, res: Response) {
   console.log("/ invoked");
-  res.send('Hola mundo!');
+  res.send("Hola mundo!");
 }
